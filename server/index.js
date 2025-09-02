7344d03bb0c2066bfb7f5ad4a04b0487942b3053
Intakhab2004 <ikbalam4648@gmail.com>
2025-09-02 12:23:08 +0530

    Cors cofig changed

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const allowedOrigins = [
+    "https://study-notion-intakhab-alams-projects-605e382d.vercel.app",
     "https://study-notion-nu-flax.vercel.app", // Production Frontend
     "http://localhost:3000" // Local Frontend (React default)
 ];
